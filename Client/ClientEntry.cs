81080a0858e0b60b08153987303e79a96e53d7b7
Airwarfare <jordanscast@gmail.com>
2019-03-21 17:57:21 -0500

    Preprocessor defines for client and server when building

---
+++ Client/ClientEntry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if !UNITY_SERVER
+public class ClientEntry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I'm the client");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+#endif
---
+++ Server/ServerEntry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Preproccesor defines for the server side only code
+//Build in server mode for this
+#if UNITY_SERVER
+public class ServerEntry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I'm the server");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+#endif
